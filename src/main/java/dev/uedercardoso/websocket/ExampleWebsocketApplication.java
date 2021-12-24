39856fd045c05107613a131687a5f668046f6410
uedermdscardoso <ueder.mds.cardoso@gmail.com>
2021-12-24 18:02:17 -0300

    save

---
+++ src/main/java/dev/uedercardoso/websocket/ExampleWebsocketApplication.java
@@ -0,0 +1,13 @@
+package dev.uedercardoso.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dev/uedercardoso/websocket/web/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package dev.uedercardoso.websocket.web.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.HtmlUtils;
+
+import dev.uedercardoso.websocket.web.domain.model.Greeting;
+import dev.uedercardoso.websocket.web.domain.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+  @MessageMapping("/hello")
+  @SendTo("/topic/greetings")
+  public Greeting greeting(@RequestBody HelloMessage message) throws Exception {
+    Thread.sleep(1000);
+    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
+
+}
