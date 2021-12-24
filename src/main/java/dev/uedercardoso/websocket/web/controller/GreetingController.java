299908078918c536e789c66b6655145b045fa108
uedermdscardoso <ueder.mds.cardoso@gmail.com>
2021-12-24 20:09:44 -0300

    save

--- src/main/java/dev/uedercardoso/websocket/web/controller/GreetingController.java
+++ src/main/java/dev/uedercardoso/websocket/web/controller/GreetingController.java
@@ -16,7 +16,7 @@ public class GreetingController {
 		
   @MessageMapping("/hello")
   @SendTo("/topic/greetings")
-  public Greeting greeting(@RequestBody HelloMessage message) throws Exception {
+  public Greeting greeting(HelloMessage message) throws Exception {
     Thread.sleep(1000);
     return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
   }
