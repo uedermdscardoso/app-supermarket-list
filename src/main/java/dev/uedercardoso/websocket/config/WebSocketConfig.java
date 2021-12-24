8709f874af3f3e17efa587216957771d37ce5210
uedermdscardoso <ueder.mds.cardoso@gmail.com>
2021-12-24 20:55:34 -0300

    save

--- src/main/java/dev/uedercardoso/websocket/config/WebSocketConfig.java
+++ src/main/java/dev/uedercardoso/websocket/config/WebSocketConfig.java
@@ -18,7 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    registry.addEndpoint("/gs-guide-websocket")
+    	.setAllowedOrigins("*")
+    	.withSockJS();
   }
 
 }
