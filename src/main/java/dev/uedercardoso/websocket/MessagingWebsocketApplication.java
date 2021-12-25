1645743c550e723873cf52c9851ea462c724656a
uedermdscardoso <ueder.mds.cardoso@gmail.com>
2021-12-24 22:10:35 -0300

    save update all

---
+++ src/main/java/dev/uedercardoso/websocket/MessagingWebsocketApplication.java
@@ -0,0 +1,13 @@
+package dev.uedercardoso.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingWebsocketApplication.class, args);
+	}
+
+}
